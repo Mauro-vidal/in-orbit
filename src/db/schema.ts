2e6aac1a8f75a868bc5d3c5fc8464f4f6292d58e
Mauro-vidal <mauroviidal@gmail.com>
2024-09-12 00:29:44 -0300

    configurações realizadas e inicio da criação de tabelas par bd.

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { text, integer, timestamp } from 'drizzle-orm/pg-core'
+import { pgTable, PgTable } from 'drizzle-orm/pg-core'
+
+export const goals = pgTable('goals', {
+  id: text('id').primaryKey(),
+  title: text('title').notNull(),
+  desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
