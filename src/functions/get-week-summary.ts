351c9ed6123af9e317ba4c611c44a1f6f1f37b2e
Mauro-vidal <mauroviidal@gmail.com>
2024-09-18 00:06:38 -0300

    inicio construção de query para contagem de metas conconcluidas na semana.

--- src/functions/get-week-summary.ts
+++ src/functions/get-week-summary.ts
@@ -1,4 +1,42 @@
+import { and, count, eq, gte, lte } from 'drizzle-orm'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import dayjs from 'dayjs'
+
 export async function getWeekSummary() {
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+  const lastDayOfWeek = dayjs().endOf('week').toDate()
+
+  /*query para saber quantas metas precisa completar na semana*/
+  const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWeek))
+  )
+
+  /*Metas já completas*/
+  const goalCompletionCounts = db.$with('goal_completion_counts').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id).as('completionCount'),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goalCompletions.createdAt, firstDayOfWeek),
+          lte(goalCompletions.createdAt, lastDayOfWeek)
+        )
+      )
+      .groupBy(goalCompletions.goalId)
+  )
+
   return {
     summary: 'teste',
   }
